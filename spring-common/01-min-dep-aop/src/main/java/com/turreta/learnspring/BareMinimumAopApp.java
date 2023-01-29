44e5951fd78d2a06dc83bae5787d41a94c24dad8
karl <turretasolutions@gmail.com>
2023-01-29 15:15:39 +0800

    Dev

--- spring-common/01-min-dep-aop/src/main/java/com/turreta/learnspring/BareMinimumAopApp.java
+++ spring-common/01-min-dep-aop/src/main/java/com/turreta/learnspring/BareMinimumAopApp.java
@@ -5,7 +5,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
-
 public class BareMinimumAopApp {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
